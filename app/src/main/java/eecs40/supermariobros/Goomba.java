52894bc08c587f1dbc1cb1075e24d7fc327c5071
Alex Tran <tranad2@uci.edu>
2015-05-09 18:20:30 -0700

    Added Goomba

---
+++ app/src/main/java/eecs40/supermariobros/Goomba.java
@@ -0,0 +1,20 @@
+package eecs40.supermariobros;
+
+import android.graphics.Bitmap;
+import android.graphics.Rect;
+
+/**
+ * Created by Alex on 5/9/2015.
+ */
+public class Goomba extends Sprite {
+
+    Bitmap bitmapGoomba;
+
+    public Goomba(float x, float y){
+        //Load images
+
+        //Design hitbox
+        super(x,y);
+    }
+
+}
