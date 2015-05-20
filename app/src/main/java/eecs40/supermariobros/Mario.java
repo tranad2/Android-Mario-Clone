702c4016384e3a19916210b0e4b72fd48bb7afaa
Michael Herrera <michaerh@uci.edu>
2015-05-20 00:08:58 -0700

    Forgot to set mushroom back to normal

--- app/src/main/java/eecs40/supermariobros/Mario.java
+++ app/src/main/java/eecs40/supermariobros/Mario.java
@@ -537,7 +537,7 @@ public class Mario extends Sprite implements TimeConscious {
             Item item = items.get(i);
             if(dst.intersect(item.getRect())){
                 if(item.type == 0){//Mushroom
-                    setForm(2);
+                    setForm(1);
                     view.score += 1000;
                     item.die();
                 }else if(item.type == 1){//Fire Flower
